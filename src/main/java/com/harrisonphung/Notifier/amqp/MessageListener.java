32b8dc14197378e280ad08a452be514753039c50
Harrison Phung <hphung3@gatech.edu>
2018-08-04 17:50:26 -0400

    adding rabbitmq classes to be used to listen to messages to send out

---
+++ src/main/java/com/harrisonphung/Notifier/amqp/MessageListener.java
@@ -0,0 +1,7 @@
+package com.harrisonphung.Notifier.amqp;
+
+/**
+ * Created by harrisonphung on 8/1/18.
+ */
+public class MessageListener {
+}
---
+++ src/main/java/com/harrisonphung/Notifier/amqp/MessageListenerConfig.java
@@ -0,0 +1,13 @@
+package com.harrisonphung.Notifier.amqp;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by harrisonphung on 8/1/18.
+ */
+@Component
+public class MessageListener {
+
+
+
+}
